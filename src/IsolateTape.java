3e11f19428a027e90f5e479f49a9cf2b6eb25e4f
EvanYap <evanyap.14@gmail.com>
2017-01-12 19:15:06 -0800

    Make test class

--- src/IsolateTape.java
+++ src/IsolateTape.java
@@ -3,66 +3,50 @@ import java.awt.image.BufferedImage;
 import boofcv.alg.color.ColorHsv;
 import boofcv.gui.image.ShowImages;
 import boofcv.io.image.ConvertBufferedImage;
-import boofcv.io.image.UtilImageIO;
 import boofcv.struct.image.GrayF32;
 import boofcv.struct.image.Planar;
 
 public class IsolateTape {
 
-  private static BufferedImage image = UtilImageIO
-      .loadImage("imgs/goal4.png");
+	public static void showSelectedColor(String name, BufferedImage image, float hue, float minHue, float maxHue,
+			float minValue, float maxValue) {
+		Planar<GrayF32> input = ConvertBufferedImage.convertFromMulti(image, null, true, GrayF32.class);
+		Planar<GrayF32> hsv = input.createSameShape();
 
-  public static void main(String[] args) {
-    showSelectedColor("image", image, 90);
-  }
+		// Convert into HSV
+		ColorHsv.rgbToHsv_F32(input, hsv);
 
-  public static void showSelectedColor(String name, BufferedImage image,
-      float hue) {
-    Planar<GrayF32> input = ConvertBufferedImage.convertFromMulti(image, null,
-        true, GrayF32.class);
-    Planar<GrayF32> hsv = input.createSameShape();
+		// Extract hue and value bands which are independent of saturation
+		GrayF32 H = hsv.getBand(0);
+		GrayF32 V = hsv.getBand(2);
+		// Adjust the relative importance of Hue and Saturation.
 
-    // Convert into HSV
-    ColorHsv.rgbToHsv_F32(input, hsv);
+		// step through each pixel and mark how close it is to the selected
+		// color
+		BufferedImage output = new BufferedImage(input.width, input.height, BufferedImage.TYPE_INT_RGB);
+		for (int y = 0; y < hsv.height; y++) {
+			for (int x = 0; x < hsv.width; x++) {
+				// Hue is an angle in radians, so simple subtraction doesn't
+				// work
+				float dh = H.unsafe_get(x, y);
+				float dv = V.unsafe_get(x, y);
 
-    // Euclidean distance squared threshold for deciding which pixels are
-    // members of the selected set
-    float minDist1 = 60;
-    float maxDist1 = 180;
-    float maxDist2 = 70;
+				// this distance measure is a bit naive, but good enough for to
+				// demonstrate the concept
+				float hueDist = (float) ((dh * 180) / Math.PI);
+				float valueDist = dv;
 
-    // Extract hue and value bands which are independent of saturation
-    GrayF32 H = hsv.getBand(0);
-    GrayF32 V = hsv.getBand(2);
-    // Adjust the relative importance of Hue and Saturation.
+				// System.out.println(dist);
 
-    // step through each pixel and mark how close it is to the selected
-    // color
-    BufferedImage output = new BufferedImage(input.width, input.height,
-        BufferedImage.TYPE_INT_RGB);
-    for (int y = 0; y < hsv.height; y++) {
-      for (int x = 0; x < hsv.width; x++) {
-        // Hue is an angle in radians, so simple subtraction doesn't
-        // work
-        float dh = H.unsafe_get(x, y);
-        float dv = V.unsafe_get(x, y);
+				if (valueDist >= minValue && valueDist <= maxValue) {
+					if (minHue <= hueDist && maxHue >= hueDist) {
+						output.setRGB(x, y, image.getRGB(x, y));
+					}
+				}
+			}
+		}
 
-        // this distance measure is a bit naive, but good enough for to
-        // demonstrate the concept
-        float dist = (float) ((dh * 180) / Math.PI);
-        float dist2 = dv;
-
-        // System.out.println(dist);
-
-        if (dist2 >= maxDist2) {
-          if (minDist1 <= dist && maxDist1 >= dist) {
-            output.setRGB(x, y, image.getRGB(x, y));
-          }
-        }
-      }
-    }
-
-    ShowImages.showWindow(output, "Showing " + name);
-  }
+		ShowImages.showWindow(output, "Showing " + name);
+	}
 
 }
---
+++ src/TestImage.java
@@ -0,0 +1,11 @@
+import java.awt.image.BufferedImage;
+
+import boofcv.io.image.UtilImageIO;
+
+public class TestImage {
+	public static void main(String[] args) {
+		BufferedImage image = UtilImageIO.loadImage("imgs/goal4.png");
+		IsolateTape.showSelectedColor("image", image, 90, 60f, 180f, 40f, 70f);
+	}
+
+}

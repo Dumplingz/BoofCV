0e7087a558d956916fba0e3e0f3d631f93f19d3a
EvanYap <evanyap.14@gmail.com>
2017-01-12 19:28:16 -0800

    Find values to isolate goal4.png (dark)

--- src/TestImage.java
+++ src/TestImage.java
@@ -5,7 +5,6 @@ import boofcv.io.image.UtilImageIO;
 public class TestImage {
 	public static void main(String[] args) {
 		BufferedImage image = UtilImageIO.loadImage("imgs/goal4.png");
-		IsolateTape.showSelectedColor("image", image, 90, 60f, 180f, 70f, 180f);
+		IsolateTape.showSelectedColor("image", image, 90, 100f, 180f, 70f, 160f);
 	}
-
 }

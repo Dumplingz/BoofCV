1882087492f6ebe4537fc4c9bb6f8676571a6193
Christopher Zhu <christopherzhu@Christophers-MacBook-Pro.local>
2017-01-24 19:02:32 -0800

    add new images

--- src/TestImage.java
+++ src/TestImage.java
@@ -1,10 +1,37 @@
 import java.awt.image.BufferedImage;
 
+import boofcv.alg.shapes.polygon.BinaryPolygonDetector;
+import boofcv.factory.shape.ConfigPolygonDetector;
+import boofcv.factory.shape.FactoryShapeDetector;
+import boofcv.gui.ListDisplayPanel;
+import boofcv.gui.image.ShowImages;
 import boofcv.io.image.UtilImageIO;
+import boofcv.struct.image.GrayU8;
 
 public class TestImage {
-	public static void main(String[] args) {
-		BufferedImage image = UtilImageIO.loadImage("imgs/goal.png");
-		ShowImage.showImage(IsolateTape.filterSelectedHSVColor( image, 100f, 180f, 70f, 255f));
-	}
+  public static void main(String[] args) {
+    BufferedImage origImage = UtilImageIO.loadImage("imgs/goalRetake5.JPG");
+    BufferedImage image = IsolateTape.filterSelectedHSVColor(origImage, 100f,
+        180f, 70f, 255f);
+    image = IsolateTape.convertToBinaryImage(image, 0);
+
+    ListDisplayPanel panel = new ListDisplayPanel();
+
+    // first configure the detector to only detect convex shapes with 3 to 7
+    // sides
+    ConfigPolygonDetector config = new ConfigPolygonDetector(3, 7);
+    BinaryPolygonDetector<GrayU8> detector = FactoryShapeDetector
+        .polygon(config, GrayU8.class);
+
+    IsolateTape.processPolygons(image, detector, panel);
+
+    // now lets detect concave shapes with many sides
+    config.maximumSides = 12;
+    config.convex = false;
+    detector = FactoryShapeDetector.polygon(config, GrayU8.class);
+
+    IsolateTape.processPolygons(image, detector, panel);
+
+    ShowImages.showWindow(panel, "Found Polygons", true);
+  }
 }
